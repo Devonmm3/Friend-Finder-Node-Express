1c547896a76a8cb9e857e12018062fa6acd5068e
DevonMM <Devon.m.moran@gmail.com>
2019-02-11 17:43:07 -0500

    html pages

---
+++ app/data/friends.js
@@ -0,0 +1,43 @@
+var friends = [{
+    "name": "Devon",
+    "photo": "",
+    "points": [4, 2, 3, 1, 1, 3, 2, 5, 4, 3]
+}, {
+    "name": "William",
+    "photo": "",
+    "points": [2, 2, 3, 1, 5, 5, 4, 2, 3, 1]
+}, {
+    "name": "Taryn",
+    "photo": "",
+    "points": [3, 1, 5, 4, 4, 4, 2, 3, 1, 5]
+}, {
+    "name": "David",
+    "photo": "",
+    "points": [2, 3, 1, 5, 4, 3, 1, 2, 5, 2]
+}, {
+    "name": "Aimee",
+    "photo": "",
+    "points": [2, 1, 5, 5, 4, 1, 2, 1, 5, 2]
+}, {
+    "name": "Rosalind",
+    "photo": "",
+    "points": [3, 3, 2, 5, 1, 2, 3, 1, 5, 4]
+}, {
+    "name": "Michael",
+    "photo": "",
+    "points": [2, 1, 3, 4, 2, 1, 5, 4, 3, 4]
+}, {
+    "name": "Alex",
+    "photo": "",
+    "points": [2, 3, 1, 4, 5, 3, 2, 4, 1, 1]
+}, {
+    "name": "Katy",
+    "photo": "",
+    "points": [2, 1, 4, 3, 5, 2, 3, 1, 4, 3]
+}, {
+    "name": "Jessica",
+    "photo": "",
+    "points": [2, 1, 4, 3, 5, 2, 3, 1, 4, 3]
+}];
+
+module.exports = friends;
